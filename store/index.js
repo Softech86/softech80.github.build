2b9227c2b16d813246051321b318e2f23ec7befc
softech86 <softech86@163.com>
2018-04-16 20:10:55 +0800

    update(markdown): better file struct

--- store/index.js
+++ store/index.js
@@ -5,20 +5,26 @@ import { TimeFormat, plainHTML } from '~/assets/script/util.js'
 
 Vue.use(Vuex)
 
+const blogsByTime = blogs
+  .map(x => Object.assign(x, {editAt: new Date(x.createdAt || x.updatedAt || 0)}))
+  .sort((x, y) => y.editAt - x.editAt)
+  .map(x => Object.assign(x, {
+    url: `/markdown/${x.name}`,
+    editAt: new TimeFormat(x.editAt).text(),
+    body: plainHTML(x.body)
+  }))
+
+const categories = blogsByTime.reduce((s, x) => {
+  const category = s[x.category] || Object.assign(s, {[x.category]: []})[x.category]
+  category.push(x)
+  return s
+}, {})
+
 const store = () => new Vuex.Store({
 
   state: {
-    blogs: blogs
-      .map(x => Object.assign(x, {editAt: new Date(x.createdAt || x.updatedAt || 0)}))
-      .sort((x, y) => x.editAt < y.editAt)
-      .map(x => {
-        Object.assign(x, {
-          url: `/markdown/${x.name}`,
-          editAt: new TimeFormat(x.editAt).text(),
-          body: plainHTML(x.body)
-        })
-        return x
-      })
+    blogs: blogsByTime,
+    categories
   },
   mutations: {
     increment (state) {
